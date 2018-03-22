5dc9414d8dfd5bcf245b7c7e9536790762d2fc54
Thomas Buckley <tbuckley@tabwebsites.com>
2018-03-22 10:47:06 -0400

    added GUI interface

--- src/com/thomasbuckley/extractor/Extractor.java
+++ src/com/thomasbuckley/extractor/Extractor.java
@@ -20,7 +20,7 @@ public class Extractor {
 	 // This is a fake USER_AGENT so the web server thinks the robot is a normal web browser.
     private static final String USER_AGENT =
             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
-    private static final int MAX_DEPTH = 2;
+    private static final int MAX_DEPTH = 6;
     private HashSet<String> links;
     private static ArrayList<String> listOfLinks;
     private String genre;
@@ -71,7 +71,7 @@ public class Extractor {
                     
                     
                 } else {
-                	System.out.println("Falure finding " + genre + " music.");
+                	System.out.println("Failure finding " + genre + " music.");
                     for (Element page : linksOnPage) {
                         
                         getPageLinks(page.attr("abs:href"), depth, genre);
--- src/com/thomasbuckley/extractor/Main.java
+++ src/com/thomasbuckley/extractor/Main.java
@@ -1,14 +1,35 @@
 package com.thomasbuckley.extractor;
 
-public class Main {
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
 	public static String genre;
+	
+	public Main() {
+		setTitle("YouTube Music Finder");
+		setSize(300,200); // default size is 0,0
+		setLocation(200,200); // default is 0,0 (top left corner)
+		setResizable(false);
+
+		// Window Listeners
+		addWindowListener(new WindowAdapter() {
+		  	public void windowClosing(WindowEvent e) {
+			   System.exit(0);
+		  	} //windowClosing
+		} );
+	}
 	
 	// The starting point for our application
 	public static void main(String[] args) {
+	    JFrame f = new Main();
+	    f.setVisible(true);
     	Extractor extractor = new Extractor();
     	// no spaces please!
-    	Writer writer = new Writer("jazzlist");
-    	extractor.getPageLinks("https://www.youtube.com/watch?v=jqWBgiO-8Pc", 0, "jazz");
+    	Writer writer = new Writer("largefunkplaylist");
+    	extractor.getPageLinks("https://www.youtube.com/watch?v=T0ZGNGBNIL8", 0, "funk");
     	genre = extractor.getGenre();
     	writer.writeToFile(extractor.getList());
     	System.out.println("Success! Thanks for using!");
